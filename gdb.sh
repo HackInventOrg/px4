e026d4eb5fa44f7461f5b143012409b82020642d
Zakaria ElQotbi <zakaria@elqotbi.com>
2016-11-22 01:20:19 +0100

    adding gdb script

---
+++ gdb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+#===============================================================================
+#
+#          FILE:  gdb.sh
+# 
+#         USAGE:  ./gdb.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Zakaria ElQotbi (zskdan), zakaria.elqotbi@redbend.com
+#       COMPANY:  RedBend, Paris
+#       VERSION:  1.0
+#       CREATED:  20/11/2016 22:04:08 CET
+#      REVISION:  ---
+#===============================================================================
+
+[ -z "$(pidof openocd)" ] && \
+    sudo nuttx-configs/px4-same70xplained/tools/oocd.sh $PWD 
+
+cgdb -d arm-none-eabi-gdb -x nuttx-configs/px4-same70xplained/gdbinit
